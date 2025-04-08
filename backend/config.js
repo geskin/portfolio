36b798f102c662e8ae537e2acdff7243a5b88a88
Ehretz Geskin <sg3584@alum.barnard.edu>
2025-04-08 14:50:58 -0400

    Updating backend

---
+++ backend/config.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/** Shared config for application; can be required many places. */
+
+require("dotenv").config();
+require("colors");
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+
+const PORT = +process.env.PORT || 3001;
+
+// Speed up bcrypt during tests, since the algorithm safety isn't being tested
+//
+// WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
+
+console.log("TransConnect Config:".green);
+console.log("SECRET_KEY:".yellow, SECRET_KEY);
+console.log("PORT:".yellow, PORT.toString());
+console.log("---");
+
+module.exports = {
+    SECRET_KEY,
+    PORT
+};
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const app = require("./app");
-// const { PORT } = require("./config");
+const { PORT } = require("./config");
 
-app.listen(3001, "0.0.0.0", function () { //(PORT, "0.0.0.0", function () {
-    console.log(`Server running on port 3001`); //${PORT}`);
+app.listen(PORT, "0.0.0.0", function () {
+    console.log(`Server running on port ${PORT}`);
 });
